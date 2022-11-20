168f0ad824840bdd279bd041f8fbf333e47e7f1b
ka5trulkin <ka5trulkin@yandex.by>
2022-11-20 13:18:48 +0300

    feat: создать getHistory()

--- src/ru/yandex/practicum/taskTracker/interfaces/HistoryManager.java
+++ src/ru/yandex/practicum/taskTracker/interfaces/HistoryManager.java
@@ -9,5 +9,7 @@ public interface HistoryManager {
 
     void add(Task task);
 
+    void remove(int id);
+
     List<Task> getHistory();
 }
--- src/ru/yandex/practicum/taskTracker/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/taskTracker/service/InMemoryHistoryManager.java
@@ -20,6 +20,11 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
     public List<Task> getHistory() {
         return history;
     }
