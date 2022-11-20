76434630b9f98cc77a62496179c81a800060ac4f
ka5trulkin <ka5trulkin@yandex.by>
2022-11-20 13:21:18 +0300

    feat: создать CustomLinkedList

--- src/ru/yandex/practicum/taskTracker/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/taskTracker/service/InMemoryHistoryManager.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.taskTracker.service;
 import ru.yandex.practicum.taskTracker.interfaces.HistoryManager;
 import ru.yandex.practicum.taskTracker.model.Task;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final LinkedList<Task> history = new LinkedList<>();
@@ -28,4 +30,26 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
         return history;
     }
+}
+
+class CustomLinkedList<T> {
+    public Node<T> head;
+    public Node<T> tail;
+    private int size = 0;
+    public Map<Integer, Node<T>> nodeList = new HashMap<>();
+
+    void linkLast(T data, int id) {
+        final Node<T> t = tail;
+        final Node<T> newNode = new Node<>(t, data, null);
+        nodeList.put(id, newNode);
+        tail = newNode;
+        if (t == null) {
+            head = newNode;
+        } else {
+            t.next = newNode;
+        }
+        size++;
+    }
+
+    void removeNode()
 }
