6627fa49e2aa86f841c8cca7d18281c595f858f5
ka5trulkin <ka5trulkin@yandex.by>
2022-11-23 17:47:31 +0300

    fix: изменить код по комментариям

---
+++ src/ru/yandex/practicum/taskTracker/service/CustomLinkedList.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.taskTracker.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class CustomLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+    private final Map<Integer, Node<T>> nodeMap = new HashMap<>();
+
+    void linkLast(T data, int id) {
+        final Node<T> prevNode = tail;
+        final Node<T> newNode = new Node<>(prevNode, data, null);
+
+        nodeMap.put(id, newNode);
+        tail = newNode;
+        if (prevNode == null) {
+            head = newNode;
+        } else {
+            prevNode.next = newNode;
+        }
+        size++;
+    }
+
+    private void removeNode(Node<T> node) {
+        if (node == head) {
+            if (node.next != null) {
+                head = node.next;
+                head.prev = null;
+                if (head.next == null) {
+                    tail = null;
+                }
+            } else {
+                head = null;
+                tail = null;
+            }
+        } else if (node == tail) {
+            tail = tail.prev;
+            tail.next = null;
+            if (head == tail) {
+                tail = null;
+            }
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+        size--;
+    }
+
+    public void remove(int id) {
+        if (nodeMap.get(id) != null) {
+            Node<T> node = nodeMap.get(id);
+
+            removeNode(node);
+            nodeMap.remove(id);
+        }
+    }
+
+    List<T> toArrayList() {
+        List<T> result = new ArrayList<>();
+        Node<T> node = head;
+
+        while (node != null) {
+            result.add(node.data);
+            node = node.next;
+        }
+        return result;
+    }
+
+    private static class Node<T> {
+        final T data;
+        Node<T> next;
+        Node<T> prev;
+
+        public Node(Node<T> prev, T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
--- src/ru/yandex/practicum/taskTracker/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/taskTracker/service/InMemoryHistoryManager.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.taskTracker.service;
 import ru.yandex.practicum.taskTracker.interfaces.HistoryManager;
 import ru.yandex.practicum.taskTracker.model.Task;
 
-import java.util.*;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final CustomLinkedList<Task> history = new CustomLinkedList<>();
@@ -14,16 +14,13 @@ public class InMemoryHistoryManager implements HistoryManager {
             int idTask = task.getId();
 
             remove(idTask);
-            history.linkLast(task, task.getId());
+            history.linkLast(task, idTask);
         }
     }
 
     @Override
     public void remove(int id) {
-        if (history.getNodeById(id) != null) {
-            history.removeNode(history.getNodeById(id));
-            history.getNodeMap().remove(id);
-        }
+        history.remove(id);
     }
 
     @Override
@@ -36,99 +33,5 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public List<Task> getHistory() {
         return history.toArrayList();
-    }
-}
-
-class CustomLinkedList<T> {
-    private Node<T> head;
-    private Node<T> tail;
-    private int size = 0;
-    private final Map<Integer, Node<T>> nodeMap = new HashMap<>();
-
-    void linkLast(T data, int id) {
-        final Node<T> prevNode = tail;
-        final Node<T> newNode = new Node<>(prevNode, data, null);
-
-        nodeMap.put(id, newNode);
-        tail = newNode;
-        if (prevNode == null) {
-            head = newNode;
-        } else {
-            prevNode.next = newNode;
-        }
-        size++;
-    }
-
-    void removeNode(Node<T> node) {
-        if (node == head) {
-            if (node.next != null) {
-                head = node.next;
-                head.prev = null;
-                if (head.next == null) {
-                    tail = null;
-                }
-            } else {
-                head = null;
-                tail = null;
-            }
-        } else if (node == tail) {
-            tail = tail.prev;
-            tail.next = null;
-            if (head.equals(tail)) {
-                head.next = null;
-                tail = null;
-            }
-        } else {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-        }
-        size--;
-    }
-
-    public Map<Integer, Node<T>> getNodeMap() {
-        return nodeMap;
-    }
-
-    Node<T> getNodeById(int id) {
-        return nodeMap.get(id);
-    }
-
-    boolean contains(Integer id) {
-        return nodeMap.containsKey(id);
-    }
-
-    public Node<T> getHead() {
-        return head;
-    }
-
-    public Node<T> getTail() {
-        return tail;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    List<T> toArrayList() {
-        List<T> result = new ArrayList<>();
-        Node<T> node = head;
-
-        while (node != null) {
-            result.add(node.data);
-            node = node.next;
-        }
-        return result;
-    }
-
-    private static class Node <T> {
-        private final T data;
-        private Node<T> next;
-        private Node<T> prev;
-
-        public Node(Node<T> prev, T data, Node<T> next) {
-            this.data = data;
-            this.next = next;
-            this.prev = prev;
-        }
     }
 }
