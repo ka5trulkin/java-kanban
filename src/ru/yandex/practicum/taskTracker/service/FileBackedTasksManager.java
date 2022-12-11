61a218af68cc874a08c7e290c19138cab60b0fe3
ka5trulkin <ka5trulkin@yandex.by>
2022-12-11 18:57:27 +0300

    feat: создать main()

--- src/ru/yandex/practicum/taskTracker/service/FileBackedTasksManager.java
+++ src/ru/yandex/practicum/taskTracker/service/FileBackedTasksManager.java
@@ -14,14 +14,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileBackedTasksManager extends InMemoryTaskManager{
+public class FileBackedTasksManager extends InMemoryTaskManager {
     Path backupFile;
-
-    public static void main(String[] args) {
-//        Path path = Paths.get("src/ru/yandex/practicum/taskTracker/files/backup-task-manager.csv");
-//        FileBackedTasksManager tasksManager1 = FileBackedTasksManager.loadFromFile(path);
-
-    }
 
     private void save() {
         if (!Files.isDirectory(backupFile)) {
@@ -124,6 +118,13 @@ public class FileBackedTasksManager extends InMemoryTaskManager{
         this.subtasks.put(key, task);
     }
 
+    private Type getType(String backupFileLine) {
+        int firstIndex = backupFileLine.indexOf(",") + 1;
+        int secondIndex = backupFileLine.indexOf(",", firstIndex);
+        String type = backupFileLine.substring(firstIndex, secondIndex);
+        return Type.valueOf(type);
+    }
+
     public static FileBackedTasksManager loadFromFile(Path backupFile) {
         FileBackedTasksManager tasksManager = new FileBackedTasksManager(backupFile);
         String[] fileLines;
@@ -150,13 +151,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager{
         return tasksManager;
     }
 
-    private Type getType(String backupFileLine) {
-        int firstIndex = backupFileLine.indexOf(",") + 1;
-        int secondIndex = backupFileLine.indexOf(",", firstIndex);
-        String type = backupFileLine.substring(firstIndex, secondIndex);
-        return Type.valueOf(type);
-    }
-
     public FileBackedTasksManager(Path backupFile) {
         this.backupFile = backupFile;
     }
@@ -175,9 +169,220 @@ public class FileBackedTasksManager extends InMemoryTaskManager{
         for (Task task : manager.getHistory()) {
             historyLine.append(task.getId()).append(",");
         }
-        if (historyLine.length() > 0){
+        if (historyLine.length() > 0) {
             historyLine.deleteCharAt(historyLine.length() - 1);
         }
         return historyLine.toString();
+    }
+
+    @Override
+    public List<Task> getTasks() {
+        List<Task> result = super.getTasks();
+        save();
+        return result;
+    }
+
+    @Override
+    public List<Epic> getEpics() {
+        List<Epic> result = super.getEpics();
+        save();
+        return result;
+    }
+
+    @Override
+    public List<Subtask> getSubtasks() {
+        List<Subtask> result = super.getSubtasks();
+        save();
+        return result;
+    }
+
+    @Override
+    public void clearAllTasks() {
+        super.clearAllTasks();
+        save();
+    }
+
+    @Override
+    public void clearAllEpics() {
+        super.clearAllEpics();
+        save();
+    }
+
+    @Override
+    public void clearAllSubtasks() {
+        super.clearAllSubtasks();
+        save();
+    }
+
+    @Override
+    public Task getTaskById(int taskId) {
+        Task result = super.getTaskById(taskId);
+        save();
+        return result;
+    }
+
+    @Override
+    public Epic getEpicById(int epicId) {
+        Epic result = super.getEpicById(epicId);
+        save();
+        return result;
+    }
+
+    @Override
+    public Subtask getSubTaskById(int subtaskId) {
+        Subtask result = super.getSubTaskById(subtaskId);
+        save();
+        return result;
+    }
+
+    @Override
+    public void addNewTask(Task task) {
+        super.addNewTask(task);
+        save();
+    }
+
+    @Override
+    public void addNewEpic(Epic epic) {
+        super.addNewEpic(epic);
+        save();
+    }
+
+    @Override
+    public void addNewSubtask(Subtask subtask) {
+        super.addNewSubtask(subtask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubtask(Subtask subtask) {
+        super.updateSubtask(subtask);
+        save();
+    }
+
+    @Override
+    public void removeTaskById(int taskId) {
+        super.removeTaskById(taskId);
+        save();
+    }
+
+    @Override
+    public void removeEpicById(int epicId) {
+        super.removeEpicById(epicId);
+        save();
+    }
+
+    @Override
+    public void removeSubtaskById(int subtaskId) {
+        super.removeSubtaskById(subtaskId);
+        save();
+    }
+
+    public static void main(String[] args) {
+        Path path = Paths.get("src/ru/yandex/practicum/taskTracker/files/backup-task-manager.csv");
+        FileBackedTasksManager taskManager = FileBackedTasksManager.loadFromFile(path);
+
+        Task task1 = new Task(
+                "Просто задача", "Просто Мария создала просто задачу", taskManager.setId());
+        taskManager.addNewTask(task1);
+        Epic epic1 = new Epic(
+                "Исправить код", "Исправить все ошибки выявленные при ревью", taskManager.setId());
+        taskManager.addNewEpic(epic1);
+        Subtask subtask1 = new Subtask(
+                "Исправить класс Task", "Тут могла быть ваша реклама!", taskManager.setId()
+                , 2);
+        taskManager.addNewSubtask(subtask1);
+        Subtask subtask2 = new Subtask(
+                "Исправить класс Epic", "Сегодня действуют скидки на рекламу!", taskManager.setId()
+                , 2);
+        taskManager.addNewSubtask(subtask2);
+        Subtask subtask3 = new Subtask("Исправить класс Subtask",
+                "Миллиарды уже купили нашу рекламу почему не Вы?", taskManager.setId(), 2);
+        taskManager.addNewSubtask(subtask3);
+        Epic epic2 = new Epic("Проверить второй эпик",
+                "Проверить работоспособность второго эпика", taskManager.setId());
+        taskManager.addNewEpic(epic2);
+        Subtask subtask4 = new Subtask("Проверить подзадачу № 1",
+                "Описание подзадачи № 1", taskManager.setId(), 6);
+        taskManager.addNewSubtask(subtask4);
+        Subtask subtask5 = new Subtask("Проверить подзадачу № 2",
+                "Описание подзадачи № 2", taskManager.setId(), 6);
+        taskManager.addNewSubtask(subtask5);
+        Subtask subtask6 = new Subtask("Проверить подзадачу № 3",
+                "Описание подзадачи № 3", taskManager.setId(), 6);
+        taskManager.getSubTaskById(9);
+        taskManager.getSubTaskById(8);
+        taskManager.getSubTaskById(7);
+        taskManager.getEpicById(6);
+        taskManager.getSubTaskById(5);
+        taskManager.getSubTaskById(3);
+        taskManager.getEpicById(2);
+        taskManager.getTaskById(1);
+        System.out.println(System.lineSeparator()
+                + "Вывод данных после заполнения задач:" + System.lineSeparator()
+                + "ID задач из истории просмотров:");
+        for (Task task : taskManager.historyManager.getHistory()) {
+            System.out.print(task.getId() + " ");
+        }
+        System.out.println();
+        for (Task task : taskManager.getHistoryFromManager()) {
+            System.out.println(task);
+        }
+        taskManager.addNewSubtask(subtask6);
+        taskManager.getSubTaskById(3).setStatus(Status.IN_PROGRESS);
+        taskManager.getSubTaskById(3).setStatus(Status.DONE);
+        taskManager.getSubTaskById(3).setStatus(Status.IN_PROGRESS);
+        taskManager.getSubTaskById(5).setStatus(Status.DONE);
+        taskManager.getSubTaskById(9);
+        taskManager.getSubTaskById(7);
+        taskManager.getSubTaskById(8);
+        taskManager.getEpicById(6);
+        taskManager.getEpicById(2);
+        taskManager.getTaskById(1);
+        taskManager.getSubTaskById(9);
+        taskManager.getSubTaskById(7);
+        taskManager.getSubTaskById(8);
+        taskManager.getEpicById(6);
+        taskManager.getEpicById(2);
+        taskManager.getTaskById(1);
+        taskManager.getSubTaskById(9);
+        taskManager.getSubTaskById(7);
+        taskManager.getSubTaskById(8);
+        taskManager.getEpicById(6);
+        taskManager.getEpicById(2);
+        System.out.println(System.lineSeparator() + "ID задач после вызова:");
+        for (Task task : taskManager.historyManager.getHistory()) {
+            System.out.print(task.getId() + " ");
+        }
+        System.out.println();
+        FileBackedTasksManager taskManager2 = new FileBackedTasksManager(path);
+
+        System.out.println(System.lineSeparator() + "ID задач из истории просмотров нового объекта:");
+        for (Task task : taskManager2.historyManager.getHistory()) {
+            System.out.print(task.getId() + " ");
+        }
+        System.out.println();
+        FileBackedTasksManager taskManager3 = FileBackedTasksManager.loadFromFile(path);
+
+        System.out.println("ID задач из истории просмотров восстановленного файла:");
+        for (Task task : taskManager3.historyManager.getHistory()) {
+            System.out.print(task.getId() + " ");
+        }
+        System.out.println(System.lineSeparator() + "Вывод данных о задачах восстановленного файла:");
+        for (Task task : taskManager.getHistoryFromManager()) {
+            System.out.println(task);
+        }
+
     }
 }
