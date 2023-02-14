c1aac3c12ecd145557055437952cfba1b01b0832
ka5trulkin <ka5trulkin@yandex.by>
2023-02-14 21:25:25 +0300

    feat: поспать

--- src/ru/yandex/practicum/taskTracker/service/HttpTaskManager.java
+++ src/ru/yandex/practicum/taskTracker/service/HttpTaskManager.java
@@ -1,34 +1,33 @@
 package ru.yandex.practicum.taskTracker.service;
 
-import com.google.gson.Gson;
 import ru.yandex.practicum.taskTracker.http.KVTaskClient;
-import ru.yandex.practicum.taskTracker.model.Task;
+import ru.yandex.practicum.taskTracker.model.Epic;
 
-import java.awt.*;
-import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.Arrays;
 
 public class HttpTaskManager extends FileBackedTasksManager {
     private final KVTaskClient client;
-    private final String key = "User";
-    private final Gson gson = Managers.getGson();
-    URI uri;
+    private String key;
 
     public HttpTaskManager(URI uri) {
-        this.uri = uri;
         this.client = new KVTaskClient(uri);
+        this.key = "default";
     }
 
     @Override
     protected void save() {
-        StringBuilder stringBuilder = new StringBuilder()
-                .append(tasksToString())
-                .append(historyToString(this.historyManager));
-        client.save(key, stringBuilder.toString());
+        String value = tasksToString() + historyToString(this.historyManager);
+        client.save(key, value);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 
     public void load() {
@@ -36,7 +35,6 @@ public class HttpTaskManager extends FileBackedTasksManager {
         String fileLine;
         String historyLine;
         int dataLine = 1;
-
         fileLines = this.client.load(this.key).split(System.lineSeparator());
         System.out.println("Проверка fileLines: " + Arrays.toString(fileLines).length());
         historyLine = fileLines[fileLines.length - 1];
@@ -51,51 +49,9 @@ public class HttpTaskManager extends FileBackedTasksManager {
         }
     }
 
-
-    public static HttpTaskManager loadFromURI(URI uri) {
-        HttpTaskManager manager = new HttpTaskManager(uri);
-        String[] fileLines;
-        String fileLine;
-        String historyLine;
-        int dataLine = 1;
-
-        fileLines = manager.client.load(manager.key).split(System.lineSeparator());
-        System.out.println("Проверка fileLines: " + Arrays.toString(fileLines).length());
-        historyLine = fileLines[fileLines.length - 1];
-        for (int index = dataLine; index < fileLines.length; index++) {
-            fileLine = fileLines[index];
-            if (!fileLine.isBlank()) {
-                manager.fillTasksManager(fileLine);
-            } else if (!historyLine.isBlank()) {
-                manager.fillHistoryManager(manager.historyFromString(historyLine));
-                break;
-            }
-        }
-        return manager;
-    }
-
-    private void getBackupFile() {
-        String data = client.load("key");
-        if (this.backupFile.isFile()) {
-            try (BufferedWriter bufferedWriter
-                         = new BufferedWriter(new FileWriter(backupFile.toString(), StandardCharsets.UTF_8))) {
-                bufferedWriter.write(data);
-            } catch (IOException e) {
-                throw new ManagerSaveException("Ошибка записи в файл");
-            }
-        }
-    }
-
     public static void main(String[] args) throws URISyntaxException {
         HttpTaskManager manager = new HttpTaskManager(new URI("http://localhost:8078"));
-        HttpTaskManager manager1 = new HttpTaskManager(new URI("http://localhost:8078"));
-        manager.addNewTask(new Task("New Task", "New Description", 1));
-//        System.out.println(manager.getTasks());
-        System.out.println(manager.client.load(manager.key));
-////        manager.deleteAllTasks();
-//        System.out.println("Удалили задачи: " + manager.getTasks());
-//        manager1 = HttpTaskManager.loadFromURI(manager1.uri);
-        manager1.load();
-        System.out.println("Должны восстановиться задачи, но это не точно! " + manager1.getTasks());
+        manager.addNewEpic(new Epic("New Epic", "Epic Description", 777));
+        System.out.println(manager.getPrioritizedTasks());
     }
 }
