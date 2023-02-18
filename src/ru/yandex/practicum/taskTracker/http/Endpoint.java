6bee1d0f084ad7f04a0400441c47836f2b09c147
ka5trulkin <ka5trulkin@yandex.by>
2023-02-18 18:02:28 +0300

    fix: исправить processRequestData()

--- src/ru/yandex/practicum/taskTracker/http/Endpoint.java
+++ src/ru/yandex/practicum/taskTracker/http/Endpoint.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.taskTracker.http;
 
+import ru.yandex.practicum.taskTracker.model.Type;
+
 public enum Endpoint {
     GET_PRIORITIZES_TASKS,
     GET_HISTORY,
@@ -19,5 +21,71 @@ public enum Endpoint {
     POST_SUBTASK,
     DELETE_ALL_SUBTASKS,
     DELETE_SUBTASK_BY_ID,
-    UNKNOWN
+    UNKNOWN;
+
+    public static Endpoint getAll(Type taskType) {
+        switch (taskType) {
+            case TASK:
+                return GET_ALL_TASKS;
+            case EPIC:
+                return GET_ALL_EPICS;
+            case SUBTASK:
+                return GET_ALL_SUBTASKS;
+            default:
+                return UNKNOWN;
+        }
+    }
+
+    public static Endpoint post(Type taskType) {
+        switch (taskType) {
+            case TASK:
+                return POST_TASK;
+            case EPIC:
+                return POST_EPIC;
+            case SUBTASK:
+                return POST_SUBTASK;
+            default:
+                return UNKNOWN;
+        }
+    }
+
+    public static Endpoint getById(Type taskType) {
+        switch (taskType) {
+            case TASK:
+                return GET_TASK_BY_ID;
+            case EPIC:
+                return GET_EPIC_BY_ID;
+            case SUBTASK:
+                return GET_SUBTASK_BY_ID;
+            default:
+                return UNKNOWN;
+        }
+    }
+
+    public static Endpoint deleteAll(Type taskType) {
+        switch (taskType) {
+            case TASK:
+                return DELETE_ALL_TASKS;
+            case EPIC:
+                return DELETE_ALL_EPICS;
+            case SUBTASK:
+                return DELETE_ALL_SUBTASKS;
+            default:
+                return UNKNOWN;
+        }
+    }
+
+    public static Endpoint deleteById(Type taskType) {
+        switch (taskType) {
+            case TASK:
+                return DELETE_TASK_BY_ID;
+            case EPIC:
+                return DELETE_EPIC_BY_ID;
+            case SUBTASK:
+                return DELETE_SUBTASK_BY_ID;
+            default:
+                return UNKNOWN;
+        }
+    }
+
 }
