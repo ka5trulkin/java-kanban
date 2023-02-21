f31f2f6e1c6e8641c73ab5209e7fe95a59345135
ka5trulkin <ka5trulkin@yandex.by>
2023-02-21 09:47:18 +0300

    fix: исправить getEndpoint()

--- src/ru/yandex/practicum/taskTracker/model/Type.java
+++ src/ru/yandex/practicum/taskTracker/model/Type.java
@@ -1,8 +1,18 @@
 package ru.yandex.practicum.taskTracker.model;
 
 public enum Type {
-    TASK,
-    EPIC,
-    SUBTASK,
-    HISTORY
+    TASK("task"),
+    EPIC("epic"),
+    SUBTASK("subtask"),
+    HISTORY("history");
+
+    final String lowerCase;
+
+    Type(String lowerCase) {
+        this.lowerCase = lowerCase;
+    }
+
+    public String toLowerCase() {
+        return lowerCase;
+    }
 }
